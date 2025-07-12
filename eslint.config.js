de9198ab1f98e65bce5eb1d56c370dd017d9184e
Gaurav Kale <kalegd@dukes.jmu.edu>
2025-07-13 00:26:49 +0300

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from "globals";
+import stylistic from '@stylistic/eslint-plugin'
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	  {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        plugins: {
+            "@stylistic": stylistic,
+        },
+  	  	rules: {
+  	  		  // Existing rules
+  	  		  semi: "error",
+  	  		  "prefer-const": "error",
+  	  		
+  	  		  // Match your existing style
+  	  		  "@stylistic/quotes": ["error", "single", { "avoidEscape": true }],
+  	  		  "@stylistic/indent": ["error", 4, { "SwitchCase": 1 }],
+  	  		  "no-trailing-spaces": "error",
+  	  		  "no-unused-vars": ["error", { "argsIgnorePattern": "^_", "caughtErrors": "none" }],
+  	  		  "no-undef": "error",
+  	  	},
+	  },
+]);
